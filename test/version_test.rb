7ea731659b879275b00ea32555d29863c8abb9a7
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-07-21 21:43:39 +0900

    Fix 'version' test to skip on memcached

--- test/version_test.rb
+++ test/version_test.rb
@@ -1,14 +1,14 @@
 require_relative "test_helper"
 
 describe "version" do
-  unless TEST_MEMCACHED
-    it "returns a version string" do
-      expect_version = `./bashcached --version`.chomp
-      with_bashcached_and_client do |client|
-        client << "version\r\n"
-        version = client.gets
-        version.must_equal "VERSION #{expect_version}\r\n"
-      end
+  it "returns a version string" do
+    skip "bashcached doesn't know memcached version" if TEST_MEMCACHED
+
+    expect_version = `./bashcached --version`.chomp
+    with_bashcached_and_client do |client|
+      client << "version\r\n"
+      version = client.gets
+      version.must_equal "VERSION #{expect_version}\r\n"
     end
   end
 end
